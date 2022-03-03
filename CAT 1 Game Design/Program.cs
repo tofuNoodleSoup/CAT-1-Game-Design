f0ed895d3d316c6d38ef249043684ff77ee13865
Tallis Beasley <bea0009@nhs.vic.edu.au>
2022-03-03 11:51:39 +1100

    changes 3/3

--- CAT 1 Game Design/Program.cs
+++ CAT 1 Game Design/Program.cs
@@ -6,8 +6,16 @@ namespace CAT1GameDesign
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, what is your name?"); //then insert something with string name = "()";
-                
+            Console.WriteLine("Hello, what is your name?");
+            Console.WriteLine("Or do you not know your name? Choose your name instead, write it below.");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Hello " + userName);
+            Console.WriteLine("It seems you have woken up in a forest, what would you like to do?");
+            Console.WriteLine("1. ");
+            Console.WriteLine("2. ");
+            Console.WriteLine("3. ");
+            Console.WriteLine("4. ");
+
         }
     }
 }
